5a9e4e46a643af13f83e4a1ea13f438203f1830f
Desz01ate <deszolate@icloud.com>
2019-04-22 23:04:30 +0700

    Add way to evaluate model, fix problem on MatrixFactorization algorithm

--- Utilities/MachineLearning/Global.cs
+++ Utilities/MachineLearning/Global.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.ML;
+﻿using Microsoft.Data.DataView;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +14,24 @@ namespace Utilities.MachineLearning
         {
             var context = new MLContext();
             context.Model.Save(model, stream);
+        }
+        public static RegressionMetrics EvaluateRegressionModel(ITransformer model, IDataView testDataframe)
+        {
+            var prediction = model.Transform(testDataframe);
+            var metrics = new MLContext().Regression.Evaluate(prediction, label: DefaultColumnNames.Label, score: DefaultColumnNames.Score);
+            return metrics;
+        }
+        public static MultiClassClassifierMetrics EvaluateMulticlassClassifierMetrics(ITransformer model,IDataView testDataframe)
+        {
+            var prediction = model.Transform(testDataframe);
+            var metrics = new MLContext().MulticlassClassification.Evaluate(prediction, label: DefaultColumnNames.Label, score: DefaultColumnNames.Score);
+            return metrics;
+        }
+        public static BinaryClassificationMetrics EvaluateBinaryClassificationMetrics(ITransformer model, IDataView testDataframe)
+        {
+            var prediction = model.Transform(testDataframe);
+            var metrics = new MLContext().BinaryClassification.Evaluate(prediction, label: DefaultColumnNames.Label, score: DefaultColumnNames.Score);
+            return metrics;
         }
     }
 }
