30e5e8fbcf13a58c9d79e7deeccf2934ae03907e
Deszolate_C <deszolate@icloud.com>
2019-05-07 14:31:50 +0700

    Clean up
    Add xml document to methods and clean up some leftover code.

--- Utilities/Cryptography.cs
+++ Utilities/Cryptography.cs
@@ -8,9 +8,20 @@ using System.Text;
 namespace Utilities
 {
     //taking the implementation based-on https://stackoverflow.com/questions/10168240/encrypting-decrypting-a-string-in-c-sharp with some different tuning parameter
+    /// <summary>
+    /// Simple wrapper for cryptography methods
+    /// </summary>
     public static class Cryptography
     {
         #region Simple Encryption - Decryption Method
+        /// <summary>
+        /// Encrypt given plain text into hash string with specific salt
+        /// </summary>
+        /// <param name="plainText">Plain text to encrypt</param>
+        /// <param name="salt">Salt using to calculate the hash</param>
+        /// <param name="blockSize">Entropy size</param>
+        /// <param name="iterations">Encryption iterations</param>
+        /// <returns></returns>
         public static string Encrypt(string plainText, string salt, int blockSize = 128, int iterations = 2000)
         {
             var saltStringBytes = GenerateRandomEntropy(blockSize);
@@ -44,6 +55,14 @@ namespace Utilities
                 }
             }
         }
+        /// <summary>
+        /// Decrypt given hash string into plain text with specific salt
+        /// </summary>
+        /// <param name="hash">Hash string to decrypt</param>
+        /// <param name="salt">Salt using to calculate the hash</param>
+        /// <param name="blockSize">Entropy size</param>
+        /// <param name="iterations">Decryption iterations</param>
+        /// <returns></returns>
         public static string Decrypt(string hash, string salt, int blockSize = 128, int iterations = 2000)
         {
             var blockSizeByte = 128 / 8;
@@ -80,7 +99,14 @@ namespace Utilities
         #endregion
 
         #region Simple Hash/Salt Generator and Verification
-
+        /// <summary>
+        /// Generate combination of hash string and salt string from given plain text and salt byte array
+        /// </summary>
+        /// <param name="plainText">Plain text to encrypt</param>
+        /// <param name="salt">Salt using to calculate the hash</param>
+        /// <param name="byteSize">Entropy size</param>
+        /// <param name="iterations">Encryption iterations</param>
+        /// <returns></returns>
         public static (string hash, string salt) GenerateHash(string plainText, byte[] salt, int byteSize = 128, int iterations = 2000)
         {
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
@@ -90,22 +116,48 @@ namespace Utilities
                 return (Convert.ToBase64String(hash), Convert.ToBase64String(salt));
             }
         }
+        /// <summary>
+        /// Generate combination of hash string and salt string from given plain text
+        /// </summary>
+        /// <param name="plainText">Plain text to encrypt</param>
+        /// <param name="byteSize">Entropy size</param>
+        /// <param name="iterations">Encryption iterations</param>
+        /// <returns></returns>
         public static (string hash, string salt) GenerateHash(string plainText, int byteSize = 128, int iterations = 2000)
         {
             return GenerateHash(plainText, GenerateSalt(), byteSize, iterations);
         }
+        /// <summary>
+        /// Generate combination of hash string and salt string from given plain text and salt base64 string
+        /// </summary>
+        /// <param name="plainText">Plain text to encrypt</param>
+        /// <param name="salt">Base64 string using to calculate the hash</param>
+        /// <param name="byteSize">Entropy size</param>
+        /// <param name="iterations">Encryption iterations</param>
+        /// <returns></returns>
         public static (string hash, string salt) GenerateHash(string plainText, string salt, int byteSize = 128, int iterations = 2000)
         {
             var saltBytes = Convert.FromBase64String(salt);
             return GenerateHash(plainText, saltBytes, byteSize, iterations);
         }
+        /// <summary>
+        /// Compare the plain text with given hash and salt
+        /// </summary>
+        /// <param name="plainText">Plain text to compare</param>
+        /// <param name="hash">Hash string to compare</param>
+        /// <param name="salt">Salt to compute while comparing</param>
+        /// <param name="iterations">Iterations to compute while comparing</param>
+        /// <returns></returns>
         public static bool Verify(string plainText, string hash, string salt, int iterations = 2000)
         {
             var inputHash = GenerateHash(plainText, salt, iterations: iterations);
             return inputHash.hash == hash;
         }
         #endregion
-
+        /// <summary>
+        /// Randomly generate salt byte array
+        /// </summary>
+        /// <returns></returns>
         public static byte[] GenerateSalt()
         {
             using (var randomNumberGenerator = new RNGCryptoServiceProvider())
