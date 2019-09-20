32e95482a49dd8f3d42cab726e1d11dedd82d78f
Deszolate_C <deszolate@icloud.com>
2019-09-20 12:42:01 +0700

    Tuning the shape of MongoDB Connector.

--- Utilities/NoSQL/MongoDB.cs
+++ Utilities/NoSQL/MongoDB.cs
@@ -15,38 +15,247 @@ using Utilities.Shared;
 
 namespace Utilities.NoSQL
 {
-    public class MongoDB //: INoSQLConnectorExtension
+    /// <summary>
+    /// Wrapper class for MongoClient instance.
+    /// </summary>
+    public class MongoDB : IDisposable
     {
-        private MongoClient _connector { get; }
-        private List<string> _databases { get; }
+        /// <summary>
+        /// Instance of underlying MongoClient.
+        /// </summary>
+        public MongoClient Connection { get; private set; }
+        /// <summary>
+        /// Active database found by internal worker.
+        /// </summary>
+        public List<string> Databases { get; }
+        /// <summary>
+        /// Connection string of current connection.
+        /// </summary>
         public string ConnectionString { get; }
-
-        public bool IsOpen => throw new NotImplementedException();
-
-        public Dictionary<SqlFunction, string> SQLFunctionConfiguration => throw new NotImplementedException();
+        /// <summary>
+        /// Active database proposed by user.
+        /// </summary>
+        public IMongoDatabase ActiveDatabase { get; private set; }
+        protected bool _disposed { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="connectionString"></param>
         public MongoDB(string connectionString)
         {
-            connectionString = connectionString;
-            _connector = new MongoClient(connectionString);
-            _databases = _connector.ListDatabases().ToList().Select(x => x["name"].ToString()).ToList();
+            ConnectionString = connectionString;
+            Connection = new MongoClient(connectionString);
+            Databases = Connection.ListDatabases().ToList().Select(x => x["name"].ToString()).ToList();
         }
+        /// <summary>
+        /// Tell the instance on what database should be active.
+        /// </summary>
+        /// <param name="databaseName"></param>
+        /// <param name="mongoDatabaseSettings"></param>
+        /// <returns></returns>
         public IMongoDatabase UseDatabase(string databaseName, MongoDatabaseSettings mongoDatabaseSettings = null)
         {
-            if (!_databases.Contains(databaseName))
-                throw new Exception($"Database '{databaseName}' is not exists in current connection (found {string.Join(",", _databases)})");
-            return _connector.GetDatabase(databaseName, mongoDatabaseSettings);
+            if (!Databases.Contains(databaseName))
+                throw new Exception($"Database '{databaseName}' is not exists in current connection (found {string.Join(",", Databases)})");
+            ActiveDatabase = Connection.GetDatabase(databaseName, mongoDatabaseSettings);
+            return ActiveDatabase;
         }
-        [Obsolete("This method will throw NotImplementedException, please use the overload one instead.")]
+        /// <summary>
+        /// Create collection on active database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="database"></param>
+        /// <returns></returns>
         public int CreateTable<T>() where T : class, new()
         {
-            throw new NotImplementedException();
-        }
-        public int CreateTable<T>(IMongoDatabase database) where T : class
-        {
             var tableName = Utilities.Shared.AttributeExtension.TableNameAttributeValidate(typeof(T));
-            database.CreateCollection(tableName);
-            _databases.Add(tableName);
+            ActiveDatabase.CreateCollection(tableName);
+            Databases.Add(tableName);
             return 1;
+        }
+        /// <summary>
+        /// Object disposer which release managed resources of this object.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                Databases.Clear();
+                Connection = null;
+                _disposed = true;
+            }
+        }
+        /// <summary>
+        /// Object disposer which release managed resources of this object.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// Verify if active database is available.
+        /// </summary>
+        protected virtual void VerifyActiveDatabase()
+        {
+            if (ActiveDatabase == null)
+            {
+                throw new NullReferenceException("Active database is not set, you must call 'UseDatabase' before attempt to execute any action");
+            }
+        }
+
+        public DeleteResult Delete<T>(T obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var primaryKey = typeof(T).PrimaryKeyAttributeValidate();
+            var field = primaryKey.Name;
+            var value = primaryKey.GetValue(obj);
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq(field, value);
+            return collection.DeleteOne(filter);
+        }
+
+        public DeleteResult Delete<T>(object primaryKey) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var pk = typeof(T).PrimaryKeyAttributeValidate();
+            var field = pk.Name;
+            var value = primaryKey;
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq(field, value);
+            return collection.DeleteOne(filter);
+        }
+
+        public DeleteResult Delete<T>(Expression<Func<T, bool>> predicate) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            return collection.DeleteOne(predicate);
+        }
+
+        public async Task<DeleteResult> DeleteAsync<T>(T obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var primaryKey = typeof(T).PrimaryKeyAttributeValidate();
+            var field = primaryKey.Name;
+            var value = primaryKey.GetValue(obj);
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq(field, value);
+            return await collection.DeleteOneAsync(filter);
+        }
+
+        public async Task<DeleteResult> DeleteAsync<T>(object primaryKey) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var pk = typeof(T).PrimaryKeyAttributeValidate();
+            var field = pk.Name;
+            var value = primaryKey;
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq(field, value);
+            return await collection.DeleteOneAsync(filter);
+        }
+
+        public async Task<DeleteResult> DeleteAsync<T>(Expression<Func<T, bool>> predicate) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            return await collection.DeleteOneAsync(predicate);
+        }
+
+
+        public int DROP_TABLE_USE_WITH_CAUTION<T>() where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            ActiveDatabase.DropCollection(table);
+            return 0;
+        }
+
+
+        public int Insert<T>(T obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            collection.InsertOne(obj);
+            return 0;
+        }
+
+        public int Insert<T>(IEnumerable<T> obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            collection.InsertMany(obj);
+            return 0;
+        }
+
+        public async Task<int> InsertAsync<T>(T obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            await collection.InsertOneAsync(obj);
+            return 0;
+        }
+
+
+        public IEnumerable<T> Select<T>(int? top = null) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var buffer = collection.Find<T>(new BsonDocument());
+            var result = top == null ? buffer.ToList() : buffer.Limit(top.Value).ToList();
+            return result;
+        }
+
+        public IEnumerable<T> Select<T>(Expression<Func<T, bool>> predicate, int? top = null) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var buffer = collection.Find<T>(predicate);
+            var result = top == null ? buffer.ToList() : buffer.Limit(top.Value).ToList();
+            return result;
+        }
+
+        public T Select<T>(object primaryKey) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq(typeof(T).PrimaryKeyAttributeValidate().Name, primaryKey);
+            var buffer = collection.Find<T>(filter);
+            return buffer.FirstOrDefault();
+        }
+
+        public ReplaceOneResult Update<T>(T obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var primaryKey = typeof(T).PrimaryKeyAttributeValidate();
+            var filter = Builders<T>.Filter.Eq(primaryKey.Name, primaryKey.GetValue(obj));
+            var result = collection.ReplaceOne(filter, obj);
+            return result;
+        }
+
+        public async Task<ReplaceOneResult> UpdateAsync<T>(T obj) where T : class, new()
+        {
+            VerifyActiveDatabase();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = ActiveDatabase.GetCollection<T>(table);
+            var primaryKey = typeof(T).PrimaryKeyAttributeValidate();
+            var filter = Builders<T>.Filter.Eq(primaryKey.Name, primaryKey.GetValue(obj));
+            var result = await collection.ReplaceOneAsync(filter, obj);
+            return result;
         }
     }
     public static class MongoDBDatabaseExtension
@@ -171,14 +380,24 @@ namespace Utilities.NoSQL
             return buffer.FirstOrDefault();
         }
 
-        public static int Update<T>(this IMongoDatabase database, T obj) where T : class, new()
+        public static ReplaceOneResult Update<T>(this IMongoDatabase database, T obj) where T : class, new()
         {
-            throw new NotImplementedException();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = database.GetCollection<T>(table);
+            var primaryKey = typeof(T).PrimaryKeyAttributeValidate();
+            var filter = Builders<T>.Filter.Eq(primaryKey.Name, primaryKey.GetValue(obj));
+            var result = collection.ReplaceOne(filter, obj);
+            return result;
         }
 
-        public static async Task<int> UpdateAsync<T>(this IMongoDatabase database, T obj) where T : class, new()
+        public static async Task<ReplaceOneResult> UpdateAsync<T>(this IMongoDatabase database, T obj) where T : class, new()
         {
-            throw new NotImplementedException();
+            var table = typeof(T).TableNameAttributeValidate();
+            var collection = database.GetCollection<T>(table);
+            var primaryKey = typeof(T).PrimaryKeyAttributeValidate();
+            var filter = Builders<T>.Filter.Eq(primaryKey.Name, primaryKey.GetValue(obj));
+            var result = await collection.ReplaceOneAsync(filter, obj);
+            return result;
         }
     }
 }
