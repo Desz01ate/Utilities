60c45b6086efba0eb42adff0278759661579fe0a
Deszolate_C <deszolate@icloud.com>
2019-09-27 18:10:25 +0700

    Add EnumeratableRepository

--- Utilities-Testing/UnrelatedTest.cs
+++ Utilities-Testing/UnrelatedTest.cs
@@ -1,35 +1,27 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Text;
-using Utilities.Testing.Models;
-using Utilities.Shared;
 using System.Linq;
-using System.Data;
-using Utilities.Interfaces;
+using Utilities.Testing.Models;
 using Utilities.Testing.SQLConnectors;
 
 namespace Utilities.Testing
 {
-    class UnrelatedTest
+    internal class UnrelatedTest
     {
-
         [Test]
         public void Playground()
         {
             var msCon = "Server=localhost;Database=Local;User=sa;Password=sa";
             var npgCon = "User Id=postgres;Password=sa;Host=localhost;Port=5432;Database=postgres";
-
-
-
-
-
-
             var affectedRow = 0;
             using (var mssqlConnector = new SQLServer(msCon))
             {
-                var d1 = mssqlConnector.Select<iris>(x => x.Label.Contains("set"));
-                var labels = new[] { "A", "B", "Iris-setosa" };
-                var data = mssqlConnector.Select<iris>(x => labels.Contains(x.Label));
+                //var d1 = mssqlConnector.Select<iris>(x => x.Label.Contains("set"));
+                //var labels = new[] { "A", "B", "Iris-setosa" }.ToList();
+                var labels = new[] { 1d, 2, 3, 4 };
+                var data = mssqlConnector.Select<iris>(x => labels.Contains(x.PetalLength)).ToList();
+                //var labels = new[] { "A", "B", "C" }.ToList();
+                //var data = mssqlConnector.Select<iris>(x => labels.Contains(x.Label));
             }
         }
     }
---
+++ Utilities.Lite/DesignPattern/UnitOfWork/Components/EnumeratableRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Utilities.Interfaces;
+
+namespace Utilities.DesignPattern.UnitOfWork.Components
+{
+    /// <summary>
+    /// Repository class with IEnumerable functionality.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TDatabase"></typeparam>
+    /// <typeparam name="TParameter"></typeparam>
+    public class EnumeratableRepository<T, TDatabase, TParameter> : Repository<T, TDatabase, TParameter>, IEnumerable<T>
+        where T : class, new()
+        where TDatabase : DbConnection, new()
+        where TParameter : DbParameter, new()
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="databaseConnector"></param>
+        public EnumeratableRepository(IDatabaseConnectorExtension<TDatabase, TParameter> databaseConnector) : base(databaseConnector)
+        {
+
+        }
+        public virtual IEnumerator<T> GetEnumerator()
+        {
+            foreach (var data in Select())
+            {
+                yield return data;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
